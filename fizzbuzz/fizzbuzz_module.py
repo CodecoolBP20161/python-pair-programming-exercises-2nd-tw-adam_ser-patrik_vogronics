ee2901987ef8934e6715bf245ecca9b8067fb713
seradam <seradam89@gmail.com>
2016-05-23 14:05:23 +0200

    classic solution

--- fizzbuzz/fizzbuzz_module.py
+++ fizzbuzz/fizzbuzz_module.py
@@ -1,8 +1,17 @@
 def fizzbuzz(number):
-    return
+    if number % 15 == 0:
+        return ("FizzBuzz")
+    elif number % 5 == 0:
+        return ("Buzz")
+    elif number % 3 == 0:
+        return ("Fizz")
+    else:
+        return number
 
 
 def main():
+    for i in range(1, 101):
+        print(fizzbuzz(i))
     return
 
 if __name__ == '__main__':
